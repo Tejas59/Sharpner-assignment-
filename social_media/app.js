182cacf3809d00342aecdcf375f4051518885a4f
Tejas59 <57625719+Tejas59@users.noreply.github.com>
2024-03-16 08:57:39 +0530

    complete working social media

--- social_media/app.js
+++ social_media/app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 
 const app = express();
 const port = 4000;
 
+// Body parser for form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(express.json());
-
-
+// Set EJS as the templating engine and adjust view directory
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+// Routes
 app.use('/', postRoutes);
 app.use('/', commentRoutes);
 
